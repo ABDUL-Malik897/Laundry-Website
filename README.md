0c99e303e0d65d3c1374eb24881291766ff58c65
ABDUL-Malik897 <malikabdulxd@gmail.com>
2026-03-07 04:19:46 +0530

    finally

--- README.md
+++
@@ -1,2 +0,0 @@
-# Laundry-Website
-Developed and deployed a responsive laundry service website using HTML, CSS, and JavaScript. Implemented DOM manipulation to manage a functional cart with add, remove, and price calculation features. Focused on clean UI, responsiveness, and smooth user interaction.
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const btns = document.querySelectorAll('.btns button[type = "submit"]')
 const tableBody = document.getElementById('addedItemsBody')
 const totalAmt  = document.getElementById('totalAmt')
@@ -15,7 +14,7 @@ btns.forEach(btn => {
             service:service,
             price:price
         })
-        console.log(cart)
+        // console.log(cart)
         if(!isAdded){
             const row = document.createElement('tr');
             row.setAttribute('data-service',service);
@@ -69,7 +68,8 @@ function bookService() {
     })
     const templateParams = {
         service_name: s_list,
-        service_price: "₹" + p_total
+        service_price: "₹" + p_total,
+        user_email: document.getElementById("email").value
     };
 
     emailjs.send("service_2y0io4r", "template_h5x6ykm", templateParams)
@@ -88,55 +88,3 @@ function bookService() {
         document.querySelector("form").reset();
         });
 }
-
-    
-=======
-const btns = document.querySelectorAll('.btns button[type = "submit"]')
-const tableBody = document.getElementById('addedItemsBody')
-const totalAmt  = document.getElementById('totalAmt')
-let total = 0
-btns.forEach(btn => {
-    btn.addEventListener('click',() =>{
-        const service = btn.dataset.service;
-        const price = Number(btn.dataset.price);
-        const isAdded = btn.dataset.added==="true";
-        if(!isAdded){
-            const row = document.createElement('tr');
-            row.setAttribute('data-service',service);
-            row.innerHTML = `
-            <td></td>
-            <td>${service}</td>
-            <td>₹${price}.00</td>
-            `
-            row.classList.add('table_body')
-            tableBody.appendChild(row);
-            total += price;
-            btn.dataset.added ="true";
-            btn.innerHTML = 'Remove Items <ion-icon name="remove-circle-outline"></ion-icon>';
-            btn.classList.add('remove_items');
-        } else {
-            const rowToRemove = tableBody.querySelector(`tr[data-service = "${service}"]`);
-            if(rowToRemove){
-                rowToRemove.remove();
-                total -= price;
-            }
-            btn.dataset.added = "false";
-            btn.innerHTML = 'Add Items <ion-icon name="add-circle-outline"></ion-icon>';
-            btn.classList.remove('remove_items');
-        }
-        updateSerialNumbers();
-        totalAmt.innerText = `₹${total}.00`;
-    });
-});
-function updateSerialNumbers(){
-    const rows = tableBody.querySelectorAll('tr');
-    rows.forEach((row, index) =>{
-        row.children[0].innerText = index + 1;
-    });
-}
-const form =document.querySelector('.booking');
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    alert("Thank you for booking the service. We will get back to you soon");
-});
->>>>>>> b96b497482094193a8ae1375f6a33c99aaf30a91
